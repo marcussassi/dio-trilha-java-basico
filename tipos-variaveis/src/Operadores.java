7c8d46a0c8da7a196b74c7910733ebd46b298027
marcussassi <marcussassicesar@gmail.com>
2024-07-08 11:37:30 -0300

    Estudo-Operadores Java

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,18 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //operador atribuição "="
+        int soma = 5 + 3;
+        System.out.println(soma);
+        String nomeCliente = "Carlos";
+        System.out.println("cliente sorteado: " +nomeCliente);
+
+        String concatenacao = 1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = "Linguagem" + " Java";
+        System.out.println(concatenacao);
+    }
+}
